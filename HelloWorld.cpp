eb844f96c485aa7f5294a08c59adde7a99623bfe
ptsouth97 <ptsouth97@gmail.com>
2022-06-05 16:49:51 -0400

    initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/************************************
+ * This program will display "Hello World!"
+ * to the screen.
+ * *********************************/
+int main()
+{
+    cout << "Hello World! This is Blake\n";
+    return 0;
+}
